d1573f1ff2e8a8e5f50b6f58f54ee5abe9bca8af
Azamat Sarkytbayev <a.sarkytbayev@gmail.com>
2017-07-17 10:42:55 +0000

    add swap function

--- file.c
+++ file.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(int argc, char **argv) {
     printf("Hello, world!\n");
     printf("This is another hello!\n");
     printf("And another hello\n");
     int x = 28;
+    int y = 0;
+    swap(&x, &y);
+    
+    printf("swapped values of (x, y) = (%d, %d)\n", x, y);
     return 0;
 }
